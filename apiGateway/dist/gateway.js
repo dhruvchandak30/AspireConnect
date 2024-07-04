4b5e53d4772ffd7e5130c37f24adc28fe18fe013
Dhruv Chandak <dhruvchandak5@gmail.com>
2024-07-04 11:33:18 +0530

    Added Token Bucket Rate Limitter

--- apiGateway/dist/gateway.js
+++ apiGateway/dist/gateway.js
@@ -6,20 +6,29 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const route_1 = __importDefault(require("./routes/route"));
 const dotenv_1 = __importDefault(require("dotenv"));
-const express_rate_limit_1 = require("express-rate-limit");
-const helmet = require("helmet");
+const helmet_1 = __importDefault(require("helmet"));
+const rate_limiter_flexible_1 = require("rate-limiter-flexible");
 dotenv_1.default.config();
-const limiter = (0, express_rate_limit_1.rateLimit)({
-    windowMs: 1 * 60 * 1000,
-    limit: 5,
-    standardHeaders: true,
-    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-});
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
-app.use(limiter);
+app.use((0, helmet_1.default)());
+const rateLimiter = new rate_limiter_flexible_1.RateLimiterMemory({
+    points: 10,
+    duration: 60,
+});
+const rateLimiterMiddleware = (req, res, next) => {
+    rateLimiter
+        //@ts-ignore
+        .consume(req.ip)
+        .then(() => {
+        next();
+    })
+        .catch(() => {
+        res.status(429).send("Too Many Requests");
+    });
+};
+app.use(rateLimiterMiddleware);
 app.use("/", route_1.default);
-app.use(helmet());
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Gateway Started on port ${port}`);
--- apiGateway/src/gateway.ts
+++ apiGateway/src/gateway.ts
@@ -1,22 +1,38 @@
 import express from "express";
 import routes from "./routes/route";
 import dotenv from "dotenv";
-import { rateLimit } from "express-rate-limit";
-const helmet = require("helmet");
+import helmet from "helmet";
+import { RateLimiterMemory } from "rate-limiter-flexible";
 
 dotenv.config();
-const limiter = rateLimit({
-  windowMs: 1 * 60 * 1000,
-  limit: 5,
-  standardHeaders: true,
-  legacyHeaders: false,
-});
 
 const app = express();
 app.use(express.json());
-app.use(limiter);
-app.use("/", routes);
 app.use(helmet());
+
+const rateLimiter = new RateLimiterMemory({
+  points: 10,
+  duration: 60,
+});
+
+const rateLimiterMiddleware = (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  rateLimiter
+    //@ts-ignore
+    .consume(req.ip)
+    .then(() => {
+      next();
+    })
+    .catch(() => {
+      res.status(429).send("Too Many Requests");
+    });
+};
+
+app.use(rateLimiterMiddleware);
+app.use("/", routes);
 
 const port = process.env.PORT || 3000;
 
